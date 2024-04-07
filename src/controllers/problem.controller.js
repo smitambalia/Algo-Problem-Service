a56f6428ce18299f6937b712a3b35fc32f9e6563
Smit Ambalia <ambaliasmit@gmail.com>
2024-04-07 16:37:40 +0530

    Feat: added not implemented APIs

--- src/controllers/problem.controller.js
+++ src/controllers/problem.controller.js
@@ -1,21 +1,35 @@
-
-
-function pingProblemController(req,res) {
-    return res.json({ message: "Problem Controller is up!" });
+function pingProblemController(req, res) {
+  return res.json({ message: "Problem Controller is up!" });
 }
-function addProblem(req, res) {}
+function addProblem(req, res) {
+  return res.status(501).json({
+    message: "Not Implemented",
+  });
+}
 
 let getProblem = (req, res) => {
-    return res.json({ message: "get problem Controller is up!" });
+  return res.status(501).json({
+    message: "Not Implemented",
+  });
 };
 
 function getProblems(req, res) {
-    return res.json({ message: "get problem Controller is up!" });
+  return res.status(501).json({
+    message: "Not Implemented",
+  });
 }
 
-function updateProblem(req, req) {}
+function updateProblem(req, req) {
+  return res.status(501).json({
+    message: "Not Implemented",
+  });
+}
 
-function deleteProblem(req, res) {}
+function deleteProblem(req, res) {
+  return res.status(501).json({
+    message: "Not Implemented",
+  });
+}
 
 module.exports = {
   addProblem,
